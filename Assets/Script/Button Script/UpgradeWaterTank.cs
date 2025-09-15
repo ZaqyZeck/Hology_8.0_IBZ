d53fe1b086977ea6ee0bb12bf0dfa19d7d5d5c35
ZaqyZeck <zaqyulunnuha@student.ub.ac.id>
2025-09-15 20:22:40 +0700

    jv
    - resolusi kecual 10/bn
    - upgrade water tank price text

--- Assets/Script/Button Script/UpgradeWaterTank.cs
+++ Assets/Script/Button Script/UpgradeWaterTank.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class UpgradeWaterTank : MonoBehaviour
@@ -5,6 +6,19 @@ public class UpgradeWaterTank : MonoBehaviour
     [SerializeField] private PlantSystem plantSystem;
 
     [SerializeField] private GameObject tutorialUI;
+
+    [SerializeField] private TextMeshPro UiText;
+    int lv;
+
+    private void OnMouseExit()
+    {
+        UiText.text = "";
+    }
+
+    private void OnMouseEnter()
+    {
+        CheckPrice();
+    }
 
     public void OnMouseOver()
     {
@@ -16,7 +30,27 @@ public class UpgradeWaterTank : MonoBehaviour
                 alurTutorial.alur[1] = true;
             } 
             plantSystem.UpgradeWaterTank();
+            CheckPrice();
             Debug.Log("terpencet");
         }
+    }
+
+    void CheckPrice()
+    {
+        lv = plantSystem.waterTankLevel;
+        switch (lv)
+        {
+            case 0:
+                lv = 200;
+                break;
+            case 1:
+                lv = 600;
+                break;
+            case 2:
+                lv = 1000;
+                break;
+        }
+
+        UiText.text = $"Upgrade for\n{lv} G";
     }
 }
